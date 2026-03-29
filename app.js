3250143293b1ef7262bb1efc8a3438196a9a4f03
sahas21212 <166435531+sahas21212@users.noreply.github.com>
2026-03-29 23:17:36 +0600

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-29T17:17:10.287Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-29T17:17:37.341Z
+Commit: Fix CSS styling bug on mobile devices
