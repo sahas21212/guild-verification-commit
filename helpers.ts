b98ccb2d06fcefe1cf303dc80339b613f06810e7
sahas21212 <166435531+sahas21212@users.noreply.github.com>
2026-03-29 23:17:54 +0600

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-29T17:17:47.784Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-29T17:17:55.380Z
+Commit: Resolve timezone issues in date handling
