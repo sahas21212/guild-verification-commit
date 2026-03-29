e267c5c0aa917de6b7b17de807f6ed00e5e3113e
sahas21212 <166435531+sahas21212@users.noreply.github.com>
2026-03-29 23:18:35 +0600

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-29T17:18:18.066Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-29T17:18:37.132Z
+Commit: Fix memory leak in event handlers
