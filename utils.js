d268d4d312ddc6f2cac084e550d2af636395ace8
sahas21212 <166435531+sahas21212@users.noreply.github.com>
2026-03-29 23:20:47 +0600

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-29T17:20:38.030Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-29T17:20:48.984Z
+Commit: Patch API endpoint error handling
