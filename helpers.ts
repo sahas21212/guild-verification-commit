c13f6f7bc520dbac0425c1879678be03067a0fbb
sahas21212 <166435531+sahas21212@users.noreply.github.com>
2026-03-29 23:22:25 +0600

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-29T17:22:11.843Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-29T17:22:26.843Z
+Commit: Fix CSS styling bug on mobile devices
